d1c2c1bc2d296062cd1edddcf1a626f81e290840
Movits <robertomovits@gmail.com>
2023-11-26 14:24:27 -0300

    agregue la api de HP

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const contenedorCards = document.querySelector("#contenedorCards");
+const API_HP = "https://hp-api.onrender.com/api/characters"
 
-function botonFetch() {
-    contenedorCards.innerHTML += `
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>`;
+async function botonFetch() {
+    const respuesta = await fetch(API_HP);
+    const datos = await respuesta.json();
+    console.log(datos);
+    datos.forEach((dato) => {
+        contenedorCards.innerHTML += `
+        <div class="card" style="width: 18rem;">
+            <img src="${dato.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${dato.name}</h5>
+                <p class="card-text">${dato.house}</p>
+            </div>
+        </div>`;
+    });
 }
